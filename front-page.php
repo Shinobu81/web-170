bc3b77710aa1a731cd9383fab95420866fc6f94b
Shinobu81 <shinobu.kinjo@gmail.com>
2017-08-11 09:28:45 -0700

    08) Create a Static Home Page with Multiple Loops for Your Site

---
+++ front-page.php
@@ -0,0 +1,35 @@
+<?php get_header(); ?>
+<!-- Begin Content -->
+<main>
+  <div class="flexslider">
+    <ul class="slides">
+      <li><img src="<?php bloginfo('template_directory')?>/images/banner-1.png" width="960" height="260" alt="Image One"></li>
+      <li><img src="<?php bloginfo('template_directory')?>/images/banner-2.png" width="960" height="260" alt="Image Two"></li>
+      <li><img src="<?php bloginfo('template_directory')?>/images/banner-3.png" width="960" height="260" alt="Image Three"></li>
+      <li><img src="<?php bloginfo('template_directory')?>/images/banner-4.png" width="960" height="260" alt="Image Four"></li>
+      <li><img src="<?php bloginfo('template_directory')?>/images/banner-5.png" width="960" height="260" alt="Image Five"></li>
+      <li><img src="<?php bloginfo('template_directory')?>/images/banner-6.png" width="960" height="260" alt="Image Six"></li>
+    </ul>
+  </div>
+  <div class="colunm">
+    <h3>About Us</h3>
+    <hr>
+    <p>International Model &amp; Toys is an importer of model kits, toys and anime apparel in the Seattle Chinatown District. Owned by Tom Dang, International Model &amp; Toys (IMT) is a family owned hobby shop, started out of passion for the modeling. We sell many imported model kits, PVC figures, blind box toys and anime related apparel. Also IMT carries many of the tools, materials and paint required for advanced kit modeling and dioramas.</p>
+  </div>
+  <div class="colunm">
+    <h3>Updates</h3>
+    <hr>
+    <p>Fusce euismod neque ut tellus volutpat fringilla. Aliquam pellentesque dui nibh, blandit rhoncus mi rutrum quis. Praesent tincidunt, felis et finibus scelerisque, velit est vehicula diam, eu accumsan velit lacus nec mi. Suspendisse mauris velit, fermentum sed fringilla vel, volutpat vel odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque aliquam risus at elit imperdiet condimentum. Etiam dictum placerat interdum.</p>
+  </div>
+  <div class="colunm">
+    <h3>Contact Info</h3>
+    <hr>
+    <p>524 S King St</p>
+    <p>Seattle, WA 98104</p>
+    <p>(206) 682-8534</p>
+    <p>Hours: Daily 11:30 - 7:30</p>
+  </div>
+</main>
+<!-- End Content -->
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,91 +1,14 @@
-<!doctype html>
-<html>
-
-
-<head>
-  <title>International Model &amp; Toys</title>
-
-  <!-- Meta tags -->
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <meta name="robots" contents="noindex, nofollow">
-
-  <!-- CSS links -->
-  <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url')?>"/>
-
-  <!-- Begin Toggle Menu -->
-  <script type="text/javascript" charset="utf-8">
-    $(window).load(function() { // enable function upon window load
-	     $("#toggle").click(function() { // when toggle is clicked...
-		       $("#topNav").toggle(); // ... open or close the navigation
-	     });
-    });
-  </script>
-  <!-- End Toggle Menu -->
-  <!-- Begin WP Head Function -->
-  <?php wp_head();?>
-  <!-- End WP Head Function -->
-
-</head>
-<body <?php body_class(); ?>>
-
-  <!-- Start Wrapper -->
-  <div id="wrapper">
-
-    <!-- Start Header -->
-    <header>
-      <img src="<?php bloginfo('template_directory')?>/images/logo.png" alt="logo" id="logo"/>
-      <h1 id="title"><?php bloginfo('name'); ?></h1><br/><small id="tagline"><?php bloginfo('description'); ?></small>
-    </header>
-    <!-- End Header -->
-
-    <!-- Start Navigation -->
-    <?php wp_nav_menu(array('theme_location' => 'main-menu', 'container' => 'div', 'container_id' => 'topNav'));?>
-    <!-- End Navigation -->
-
-<!-- Start Content -->
-<div id="content">
-  <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-    <!--<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>-->
-    <?php the_content(''); ?>
-  <?php endwhile; endif; ?>
-</div>
-<!-- End Content -->
-
-<!-- Start Sidebar -->
-<div id="sidebar">
-<?php if (is_page()) : ?>
-<h2><?php echo get_the_title($post->post_parent); ?></h2>
-
-<ul class="sub-nav-items">
-<?php
-  if ($post->post_parent){ //If we're on has a parent
-    wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => '')); // list sub pages of parent pages
-  } else {
-    wp_list_pages(array('child_of' => $post->ID, 'title_li' => ''));
-  }
-  ?>
-</ul>
-<?php endif; ?>
-<?php if(!(is_page())) : ?>
-  <h2>Blog</h2>
-<ul class="sub-nav-items">
-<?php wp_list_categories(array('title_li' => '')); ?>
-<?php endif; ?>
-</div>
-<!-- End Sidebar -->
-
-<!-- Start Footer -->
-<footer>
-  <h4 id="copywrite">International Model &amp; Toys &copy; 2017 | <em>Designed by Ronald Hamasaki</em></h4>
-</footer>
-<!--End Footer -->
-
-<!-- Begin WP Footer Function -->
-<?php wp_footer(); ?>
-<!-- End WP Footer Function -->
-
-</div>
-<!--End Wrapper-->
-
-</body>
-</html>
+<?php get_header(); ?>
+    <!-- Begin Content -->
+    <div id="content">
+    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+	  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <small>Posted on <?php the_time('F jS, Y'); ?> by <?php the_author(); ?> in <?php the_category(', '); ?></small>
+  <?php the_post_thumbnail( 'thumbnail' ); ?>
+  <?php the_excerpt(); ?>
+	<?php endwhile; endif; ?>
+	<small>index.php</small>
+    </div>
+    <!-- End Content -->
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
