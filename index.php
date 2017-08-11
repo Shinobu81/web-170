14fc834fc715d6b9249672cb979a2e71b78646f6
Shinobu81 <shinobu.kinjo@gmail.com>
2017-08-11 11:42:35 -0700

    09) Creating a Custom Posts Feed with Featured Images for Your Site

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php get_header(); ?>
-    <!-- Begin Content -->
-    <div id="content">
+<!-- Begin Content -->
+  <div id="content">
     <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-	  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <small>Posted on <?php the_time('F jS, Y'); ?> by <?php the_author(); ?> in <?php the_category(', '); ?></small>
-  <?php the_post_thumbnail( 'thumbnail' ); ?>
-  <?php the_excerpt(); ?>
+      <article class="post-excerpt"><h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <small>Posted on <?php the_time('F jS, Y'); ?> by <?php the_author(); ?> in <?php the_category(', '); ?></small>
+        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'thumbnail' ); ?></a>
+        <?php the_excerpt(); ?>
+        <p class="read-more"><a href="<?php the_permalink(); ?>">Read More </a></p>
+      </article>
 	<?php endwhile; endif; ?>
 	<small>index.php</small>
-    </div>
+  </div>
     <!-- End Content -->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
+
+
+<!-- content goes here -->
