0a9b99ba74d01d0c1eaa420531fa204eae8c3e55
Shinobu81 <shinobu.kinjo@gmail.com>
2017-08-17 08:11:00 -0700

    10) Create a Pull-Quote for Your Site Using Custom Fields

--- sidebar.php
+++ sidebar.php
@@ -1,26 +1,34 @@
 <!-- Begin Sidebar -->
 <div id="sidebar">
   <?php if(is_page()) : ?>
-    <h2 class="sub-navigation-title"><?php echo get_the_title($post->post_parent); // get the gateway page title ?></h2>
+    <!-- <h2 class="sub-navigation-title"><?php echo get_the_title($post->post_parent); // get the gateway page title ?></h2> -->
     <ul class="sub-navigation-items">
       <?php
-        if ($post->post_parent)
-        { // if the page we're on has a parent...
-          wp_list_pages(array( 'child_of' => $post->post_parent, 'title_li' => '',)); // then list sub-pages of parent page
-        }
-        else
-        { // if the page does not have a parent
-          wp_list_pages(array('child_of' => $post->ID, 'title_li' => '',)); // then list sub-pages of the page
-        }
+      if ($post->post_parent)
+      { // if the page we're on has a parent...
+        wp_list_pages(array( 'child_of' => $post->post_parent, 'title_li' => '',)); // then list sub-pages of parent page
+      }
+      else
+      { // if the page does not have a parent
+        wp_list_pages(array('child_of' => $post->ID, 'title_li' => '',)); // then list sub-pages of the page
+      }
       ?>
     </ul>
   <?php endif; ?>
-
+  
   <?php if(!(is_page())) : ?>
     <h2 class="sub-navigation-items">Blog</h2>
     <ul class="sub-navigation-items"><?php wp_list_categories(array('title_li' => '',)); ?></ul>
   <?php endif; ?>
-    
-  <?php dynamic_sidebar(1); //to call widgets ?>    
+  
+  <!-- Begin Quote -->
+  <?php if (get_post_meta($post->ID, 'Quote', true)) : //check to see if there is a quote ?>
+    <blockquote><?php echo get_post_meta($post->ID, 'Quote', true); ?></blockquote>
+  <?php endif; ?>
+  <!-- End Quote -->  
+  
+  <!-- Begin Widgets -->
+  <?php dynamic_sidebar(1); //to call widgets ?>
+  <!-- End Widgets -->
 </div>
 <!-- End Sidebar -->
